541007718bb7bc38b3b28d9fdff6a0553abe07fa
Stienaat <frank.steenhoudt@gmail.com>
2026-03-09 18:30:23 +0100

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "latinsquare-v2";
+const CACHE_NAME = "latinsquare-v6";
 
 const BASE_PATH = self.location.pathname.replace("serviceworker.js", "");
 
@@ -38,3 +38,4 @@ self.addEventListener("fetch", event => {
     })
   );
 });
+
