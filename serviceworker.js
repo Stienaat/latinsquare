a14e6a801d2f3b6694830463842cefb6f641c873
Stienaat <frank.steenhoudt@gmail.com>
2026-03-17 21:38:40 +0100

    Add files via upload

--- serviceworker.js
+++ serviceworker.js
@@ -1,52 +1,40 @@
-const CACHE_NAME = "Letterswap-v1";
+const CACHE_NAME = "latinsquare-v8";
+
+const BASE_PATH = self.location.pathname.replace("serviceworker.js", "");
 
 const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./lswap.css",
-  "./10letter.js",
-  "./strings.js",
-  "./woorden.txt",
-  "./woorden_de.txt",
-  "./woorden_fr.txt",
-  "./woorden_en.txt",
-  "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png",
-  "./images/DE.png",
-  "./images/EN.png",
-  "./images/FR.png",
-  "./images/info.png",
-  "./images/logoFS.png",
-  "./images/new.png",
-  "./images/NL.png",
-  "./images/oog.png",
-  "./images/sluiten.png",
-  "./images/taal.png"
+  "",
+  "index.html",
+  "ui.js",
+  "engine.js",
+  "lsquare.css",
+  "manifest.json",
+  "icons/icon-192.png",
+  "icons/icon-512.png",
+  "images/NL.png",
+  "images/FR.png",
+  "images/EN.png",
+  "images/DE.png",
+  "images/info.png",
+  "images/oog.png",
+  "images/taal.png",
+  "images/new.png",
+  "images/logoFS.png",
+  "images/joker.png"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE.map(file => BASE_PATH + file));
+    })
   );
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-      )
-    )
-  );
-  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(cached => {
-      return cached || fetch(event.request);
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
